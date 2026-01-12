56e62030ecb2d7f8c3d64a1e78411c02998e0b91
Musa Patel <108730442+MusaPatel21@users.noreply.github.com>
2026-01-13 00:35:41 +0530

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <link rel="icon" href="assets/image/favicon.png" />
+    <title>Aagaaz international tours travels</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content=" Qzency" />
+    <!-- GLightbox CSS & JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="assets/css/jquery-ui.css" />
+    <link rel="stylesheet" href="assets/css/boxicons.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-icons.css" />
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="assets/css/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/nice-select.css" />
+    <link rel="stylesheet" href="assets/css/daterangepicker.css" />
+    <link rel="stylesheet" href="assets/css/select2.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+
+</head>
+
+<body>
+
+    <!-- header -->
+    <?php include 'header.php'; ?>
+    <!-- header -->
+
+    <!-- breadcrumb-section start -->
+    <div class="breadcrumb-section mb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="banner-content">
+                        <h1>Gallery</h1>
+                        <ul class="breadcrumb-list">
+                            <li class="breadcrumb-item">
+                                <a href="index.php">
+                                    <div class="icon">
+                                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M17.5131 7.82508L15.4987 5.81069V0.527344C15.4987 0.236109 15.2626 0 14.9714 0H12.712C12.4207 0 12.1846 0.236109 12.1846 0.527344V2.49669L10.1746 0.486668C9.8608 0.172828 9.44356 0 8.99975 0C8.5559 0 8.13863 0.172828 7.82483 0.486668L0.486383 7.82515C-0.161477 8.47297 -0.161477 9.5271 0.486383 10.1749C0.800223 10.4888 1.21746 10.6616 1.66127 10.6616C1.75426 10.6616 1.84595 10.6535 1.93591 10.6387V16.0606C1.93591 17.13 2.80596 18 3.87538 18H7.30525C7.59649 18 7.8326 17.7639 7.8326 17.4727V13.4813H10.167V17.4727C10.167 17.7639 10.4031 18 10.6943 18H14.1242C15.1936 18 16.0636 17.13 16.0636 16.0606V10.6387C16.1535 10.6535 16.2452 10.6616 16.3382 10.6616H16.3383C16.782 10.6616 17.1992 10.4887 17.5132 10.1749C17.827 9.86105 17.9998 9.44381 17.9998 9C17.9998 8.55619 17.8269 8.13892 17.5131 7.82508ZM13.2393 1.05469H14.444V4.75604L13.2393 3.55131V1.05469ZM14.1241 16.9453H11.2216V12.9539C11.2216 12.6627 10.9855 12.4266 10.6943 12.4266H7.30522C7.01398 12.4266 6.77788 12.6627 6.77788 12.9539V16.9453H3.87534C3.38748 16.9453 2.99056 16.5484 2.99056 16.0606V10.0204L8.99975 4.01136L15.0089 10.0205V16.0606C15.0089 16.5484 14.612 16.9453 14.1241 16.9453ZM16.7674 9.42908C16.7112 9.48562 16.6443 9.53045 16.5706 9.56097C16.4969 9.5915 16.4179 9.60711 16.3382 9.6069C16.2585 9.6071 16.1795 9.5915 16.1059 9.56099C16.0322 9.53049 15.9654 9.48569 15.9092 9.42919L15.909 9.42905L9.37265 2.89269C9.16678 2.68671 8.83286 2.68678 8.62688 2.89269L2.09046 9.42901L2.09028 9.42919C1.97567 9.5438 1.82334 9.6069 1.66127 9.6069C1.4992 9.6069 1.3468 9.5438 1.23215 9.42915C0.995551 9.19255 0.995551 8.80752 1.23215 8.57092L8.5706 1.23244C8.62683 1.17591 8.69371 1.13109 8.76737 1.10058C8.84104 1.07007 8.92002 1.05447 8.99975 1.05469C9.16182 1.05469 9.31422 1.11783 9.42883 1.23244L12.3391 4.14267L16.7673 8.57085C17.004 8.80745 17.004 9.19251 16.7674 9.42908Z"
+                                                fill="white" />
+                                        </svg>
+                                    </div>
+                                    Home
+                                </a>
+                            </li>
+                            <li class="active">Gallery</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb-section end -->
+
+
+    <!-- Gallery -->
+    <section class="container py-4">
+        <div class="row g-3">
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/1.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/1.jpeg" alt="Gallery Image 1" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/2.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/2.jpeg" alt="Gallery Image 2" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/3.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/3.jpeg" alt="Gallery Image 3" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/4.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/4.jpeg" alt="Gallery Image 4" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/5.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/5.jpeg" alt="Gallery Image 5" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/6.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/6.jpeg" alt="Gallery Image 6" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/7.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/7.jpeg" alt="Gallery Image 7" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/8.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/8.jpeg" alt="Gallery Image 8" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/9.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/9.jpeg" alt="Gallery Image 9" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/10.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/10.jpeg" alt="Gallery Image 10" class="img-fluid rounded" />
+                </a>
+            </div>
+            <div class="col-6 col-md-3">
+                <a href="assets/image/gallery/11.jpeg" class="glightbox">
+                    <img src="assets/image/gallery/11.jpeg" alt="Gallery Image 11" class="img-fluid rounded" />
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- Footer  -->
+    <?php include 'footer.php'; ?>
+    <!-- Footer  -->
+
+
+    <!--  Main jQuery  -->
+    <script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery-3.7.1.min.js"></script>
+    <!-- Popper and Bootstrap JS -->
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- Swiper slider JS -->
+    <!-- Counterup JS -->
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/waypoints.min.js"></script>
+    <!-- swiper -->
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/jquery.fancybox.min.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/moment.min.js"></script>
+    <script src="assets/js/daterangepicker.min.js"></script>
+    <script src="assets/js/select2.min.js"></script>
+
+    <!-- Wow  JS -->
+    <script src="assets/js/wow.min.js"></script>
+    <!-- GSAP  JS -->
+    <script src="assets/js/gsap.min.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/glightbox/dist/js/glightbox.min.js"></script>
+    <script>
+        const lightbox = GLightbox({
+            selector: '.glightbox'
+        });
+    </script>
+</body>
+
+
+</html>
